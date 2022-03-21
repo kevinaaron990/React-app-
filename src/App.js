a56cc7f8d71c3bc0cb5d884b88a435a00785856b
kevinaaron990 <kevinhorvath39@gmail.com>
2022-03-21 19:49:31 -0300

    subiendo app react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+//import logo from './logo.svg';
+//import './App.css';
+
+
+//creamos un array con objetos 
+const todos = [
+  {text: 'ir al gym',completed:false},
+  {text: 'Cocinar almuerzo',completed:false},
+  {text: 'Estudiar',completed:false},
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //react fragmen un componente para envolver varios componentes
+    <React.Fragment>
+     <TodoCounter />
+      
+      {<TodoSearch /> }
+      
+      <TodoList>
+          
+        {todos.map(todo => (//con .map estoy renderizando mi array
+          <TodoItem key={todo.text} text={todo.text}/>//key es para identificar nuestros todos de mi array en este caso por text,y el text el contenido de cada obj de mi array 
+          
+        ))}   
+
+      </TodoList> 
+
+      <CreateTodoButton />
+
+      </React.Fragment>
   );
 }
 
---
+++ src/TodoCounter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+function TodoCounter(){
+    return(
+        <h2>Has completado 2 de 3 Todos</h2>
+    );
+}
+
+
+export { TodoCounter };
